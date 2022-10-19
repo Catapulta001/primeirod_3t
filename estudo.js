daf92af72ca1a49ce9a40992e3aa434e71c6ce1a
Catapulta001 <108942402+Catapulta001@users.noreply.github.com>
2022-10-19 10:58:43 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,13 +1,22 @@
-function total (){
-    let c = document.getElementById("valor").value;
-    let j = document.getElementById("juros").value;
-    let t = document.getElementById("meses").value;
-    let r = 0;
-    for(let i = 1; i <= t; i++){
-         r = c * ( 1 +(j/100));
-         document.write("mẽs "+ i + " valor: " + r +"<br>")
-         c = r; 
-    }
-    
-    document.write("Resultado: " + r);
+function calcula(){
+    let c = document.getElementById("valor").Value;
+    let j = document.getElementById("juros").Value;
+    let r = c * (1+ (j/200));
+document.write("Resultado: " + r);
 }
+let op = "";
+function opercao(ope){
+    op = ope;
+}
+function calcula(){
+    let c = document.getElementById(" v1 ").Value;
+    let j = document.getElementById(" v2 ").Value;
+    let r = 0;
+   
+    if(op == " + "){
+     r = Number(v1) + Number(v2);
+    
+    }
+
+
+   document.getElementById ("resultado").innerHTML = r;
